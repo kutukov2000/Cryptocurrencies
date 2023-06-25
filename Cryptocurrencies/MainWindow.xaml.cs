6f40ef9c1b76eea1bee99b78d4e77a7b6b4449db
kutukov2000 <kutukov16.2005@gmail.com>
2023-06-25 23:21:37 +0300

    Edit DataGrid

--- Cryptocurrencies/MainWindow.xaml.cs
+++ Cryptocurrencies/MainWindow.xaml.cs
@@ -28,15 +28,18 @@ namespace Cryptocurrencies
         public string rank { get; set; }
         public string symbol { get; set; }
         public string name { get; set; }
-        public string priceusd { get; set; }
+        public double priceusd { get; set; }
         //public string supply { get; set; }
         //public string maxsupply { get; set; }
-        public string marketcapusd { get; set; }
-        public string volumeusd24hr { get; set; }
+        public double marketcapusd { get; set; }
+
+        public double MarketCapB => marketcapusd / 1000000000;
+
+        //public string volumeusd24hr { get; set; }
         public string changepercent24hr { get; set; }
-        public string vwap24hr { get; set; }
+        //public string vwap24hr { get; set; }
         //public string explorer { get; set; }
-        public bool isPositive => double.Parse(changepercent24hr) > 0;
+        //public bool isPositive => double.Parse(changepercent24hr) > 0;
     }
     [AddINotifyPropertyChangedInterface]
     public class Root
